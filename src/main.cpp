fc19d3aa453de0046bc3d21230b0e3350b227e6c
akelmas <abdulkerim.elmas@agu.edu.tr>
2021-02-15 13:24:21 -0800

    Starting a Thread with Function Objects

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 
 
 #include "os.h"
+#include "vehicle.h"
 
 void print_thread_id_and_core_count();
 void example_task_to_emulate_randomness();
@@ -55,8 +56,36 @@ int main(){
         t2.detach();
     }
     std::cout<<"Multiple thread example (detach) finished (sleep_for removed)"<<std::endl;
-    
 
+
+    //EXAMPLE: passing function objects to threads
+        // create thread 
+    std::thread t3((Vehicle(1))); // C++'s most vexing parse
+
+    // do something in main()
+    std::cout << "created t3 \n";
+
+    // wait for thread to finish
+    t3.join();
+    //copy initialization
+
+    std::thread t4=  std::thread( Vehicle(2) ); 
+
+    // do something in main()
+    std::cout << "created t4(copy initialization) \n";
+
+    // wait for thread to finish
+    t4.join();
+
+   //uniform initialization
+
+    std::thread t5{Vehicle(3)}  ; 
+
+    // do something in main()
+    std::cout << "created t5(uniform initialization) \n";
+
+    // wait for thread to finish
+    t5.join();
 
     return 0;
 
